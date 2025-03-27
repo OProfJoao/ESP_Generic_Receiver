e07affe8be6df0d80c7d822b8b4135f699168fb3
João Bornelli <joao.bornelli@edu.sc.senai.br>
2025-03-27 16:00:30 -0300

    first commit

---
+++ include/env.h
@@ -0,0 +1,21 @@
+const char *WIFI_SSID = "iPhone";
+const char *WIFI_PASS = "teste123";
+
+const char *BROKER = "9ba2cb32944a4266a47c6f2a46d1bd26.s1.eu.hivemq.cloud";
+const char *MQTT_USERNAME = "hivemq.webclient.1743100343938";
+const char *MQTT_PASS = "XAy5Gn2<lgt06EFD:%*d";
+
+#ifndef WIFI_CONNECTION
+#define WIFI_CONNECTION
+
+#define WIFI_SSID WIFI_SSID
+#define WIFI_PASS WIFI_PASS
+#endif
+
+#ifndef MQTT_CONNECTION
+#define MQTT_CONNECTION
+
+#define BROKER BROKER
+#define MQTT_USERNAME MQTT_USERNAME
+#define MQTT_PASS MQTT_PASS
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,110 @@
+/*------------------------------IMPORTED LIBRARIES------------------------------*/
+#include <Arduino.h>
+#include <pubSubClient.h>
+#include <WiFiClientSecure.h>
+#include <env.h>
+
+
+/*------------------------------FUNCTION HEADERS------------------------------*/
+
+bool connectToWifi();
+bool connectToBroker();
+void messageReceived(const char* topic, byte* message, unsigned int length);
+
+/*------------------------------DEFINED VARIABLES------------------------------*/
+
+WiFiClientSecure client;
+PubSubClient mqttClient(client);
+
+const char *ledTopic = "ESP32_SENSOR/lightSensor";
+
+byte lightSensor = 34;
+byte led = 2;
+
+
+
+/*------------------------------FUNCTIONS------------------------------*/
+
+bool connectToWifi(){
+  WiFi.begin(WIFI_SSID,WIFI_PASS);
+  Serial.println("Connecting to WiFi");
+  byte tentativas;
+  while(WiFi.isConnected() == false && tentativas < 10){
+    Serial.print("Status: "); 
+    Serial.println(WiFi.status());
+    delay(1000);
+    tentativas ++;
+  }
+  if(WiFi.isConnected()){
+    Serial.println("Connected to WiFi");
+  }
+  else{
+    Serial.println("Failed to connect to WiFi");
+  }
+  return WiFi.isConnected();
+}
+
+bool connectToBroker(){
+  byte tentativas;
+  while(mqttClient.connected() == false && tentativas < 10){
+    mqttClient.setServer(BROKER,8883);
+    mqttClient.connect("ESP32_SENSOR",MQTT_USERNAME,MQTT_PASS);
+    if(mqttClient.connected() == false){
+      Serial.println("Failed to connect to broker");
+      delay(1000);
+      tentativas ++;
+    }else{
+      Serial.println("Connected to broker");
+      mqttClient.setCallback(messageReceived);
+
+      mqttClient.subscribe(ledTopic);
+    }
+    delay(1000);
+  }
+  return mqttClient.connected();
+}
+
+void messageReceived(const char* topic, byte* message, unsigned int length){
+  //Do something if receives any message
+  
+
+}
+
+
+
+/*---------------------------------MAIN LOOPS--------------------------------------*/
+
+void setup()
+{
+  client.setInsecure();
+
+  Serial.begin(9600);
+
+  connectToWifi();
+  connectToBroker();
+
+  pinMode(lightSensor,INPUT);
+  pinMode(led,OUTPUT);
+}
+
+void loop()
+{
+  if(WiFi.isConnected() == false){
+    connectToWifi();
+  }
+
+  if(mqttClient.connected() == false){
+    connectToBroker();
+  }
+
+  mqttClient.loop();
+  // byte lightSensorValue = map(analogRead(lightSensor),0,4095,0,100);
+  // Serial.println(lightSensorValue);
+  
+  // if(WiFi.isConnected() && mqttClient.connected()){
+  //   if(lightSensorValue < 80){
+  //     mqttClient.publish(ledTopic,String(lightSensorValue).c_str());
+  //   }
+  // }
+  delay(1000);
+}
