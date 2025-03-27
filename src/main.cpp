7ed57dfba853b257ed8a496364922b760ecab669
João Bornelli <joao.bornelli@edu.sc.senai.br>
2025-03-27 16:33:33 -0300

    Refactor WiFi and MQTT connection logic for improved readability and reliability

--- src/main.cpp
+++ src/main.cpp
@@ -4,100 +4,98 @@
 #include <WiFiClientSecure.h>
 #include <env.h>
 
-
 /*------------------------------FUNCTION HEADERS------------------------------*/
-
 bool connectToWifi();
 bool connectToBroker();
 void messageReceived(const char* topic, byte* message, unsigned int length);
 
 /*------------------------------DEFINED VARIABLES------------------------------*/
-
 WiFiClientSecure client;
 PubSubClient mqttClient(client);
 
-const char *ledTopic = "ESP32_SENSOR/ledControl";
+const char *ledTopic = "ESP32_SENSOR/ledControl"; // Corrigido para o tópico certo
 
 byte led = 2;
 
 /*------------------------------FUNCTIONS------------------------------*/
 
-bool connectToWifi(){
-  WiFi.begin(WIFI_SSID,WIFI_PASS);
-  Serial.println("Connecting to WiFi");
-  byte tentativas;
-  while(WiFi.isConnected() == false && tentativas < 10){
-    Serial.print("Status: "); 
+bool connectToWifi() {
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  Serial.println("Connecting to WiFi...");
+  
+  byte tentativas = 0;
+  while (!WiFi.isConnected() && tentativas < 10) {
+    Serial.print("Status: ");
     Serial.println(WiFi.status());
     delay(1000);
-    tentativas ++;
+    tentativas++;
   }
-  if(WiFi.isConnected()){
-    Serial.println("Connected to WiFi");
-  }
-  else{
+  
+  if (WiFi.isConnected()) {
+    Serial.println("Connected to WiFi!");
+  } else {
     Serial.println("Failed to connect to WiFi");
   }
   return WiFi.isConnected();
 }
 
-bool connectToBroker(){
-  byte tentativas;
-  while(mqttClient.connected() == false && tentativas < 10){
-    mqttClient.setServer(BROKER,8883);
-    mqttClient.connect("ESP32_SENSOR",MQTT_USERNAME,MQTT_PASS);
-    if(mqttClient.connected() == false){
+bool connectToBroker() {
+  byte tentativas = 0;
+  mqttClient.setServer(BROKER, 8883);
+
+  while (!mqttClient.connected() && tentativas < 10) {
+    if (mqttClient.connect("ESP32_SENSOR", MQTT_USERNAME, MQTT_PASS)) {
+      Serial.println("Connected to broker!");
+      mqttClient.setCallback(messageReceived);
+      mqttClient.subscribe(ledTopic); // Assina o tópico correto
+    } else {
       Serial.println("Failed to connect to broker");
       delay(1000);
-      tentativas ++;
-    }else{
-      Serial.println("Connected to broker");
-      mqttClient.setCallback(messageReceived);
-
-      mqttClient.subscribe(ledTopic);
+      tentativas++;
     }
-    delay(1000);
   }
   return mqttClient.connected();
 }
 
-void messageReceived(const char* topic, byte* message, unsigned int length){
-  //Do something if receives any message
+void messageReceived(const char* topic, byte* message, unsigned int length) {
   String status;
-  for (int i = 0; i<length;i++){
-    char c = (char)message[i];
-    status += c;
+  for (int i = 0; i < length; i++) {
+    status += (char)message[i];
   }
+  
+  Serial.print("Recebido do tópico ");
+  Serial.print(topic);
+  Serial.print(": ");
   Serial.println(status);
-  if(status == "Acender"){
-    digitalWrite(led,HIGH);
-  }
-  else if(status == "Apagar"){
-    digitalWrite(led,LOW);
+  
+  if (strcmp(topic, ledTopic) == 0) {
+    if (status == "Acender") {
+      digitalWrite(led, HIGH);
+      Serial.println("LED Ligado");
+    } else if (status == "Apagar") {
+      digitalWrite(led, LOW);
+      Serial.println("LED Desligado");
+    }
   }
 }
 
 /*---------------------------------MAIN LOOPS--------------------------------------*/
-
-void setup()
-{
+void setup() {
   client.setInsecure();
-
   Serial.begin(9600);
 
   connectToWifi();
   connectToBroker();
 
-  pinMode(led,OUTPUT);
+  pinMode(led, OUTPUT);
 }
 
-void loop()
-{
-  if(WiFi.isConnected() == false){
+void loop() {
+  if (!WiFi.isConnected()) {
     connectToWifi();
   }
 
-  if(mqttClient.connected() == false){
+  if (!mqttClient.connected()) {
     connectToBroker();
   }
 
